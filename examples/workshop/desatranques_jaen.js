cbe92f404e730054b47cc43c05365cc3d1026a31
Andrés <andres@foundernest.com>
2019-06-13 17:55:17 +0200

    presentation updated

---
+++ examples/workshop/desatranques_jaen.js
@@ -0,0 +1,45 @@
+const Wendigo = require('wendigo');
+
+async function main() {
+    const navigationTimeout = 30000;
+    const url = "www.desatranques.es";
+    const browser = await Wendigo.createBrowser({
+        // headless: false
+    });
+    try{
+        console.log("Open", url)
+        await browser.open(url);
+        await browser.clickText("CAMISETAS");
+        await browser.waitForNavigation(navigationTimeout);
+        console.log("In Shop");
+        const categoriesElement = await browser.findByTextContaining("#fav-nav", "Categorías")
+        await categoriesElement[0].click();
+        console.log("Sudaderas");
+        await browser.clickText("#fav-nav", "Sudaderas");
+        await browser.waitForNavigation(navigationTimeout);
+        console.log("Item View")
+        await browser.clickText(".product-container", "Sudadera con capucha", 1) // Only clicks first element
+        await browser.waitForNavigation(navigationTimeout);
+        console.log("Checkout")
+        await browser.setValue(".quantity-input", 10)
+        await browser.waitFor(".addtocart-button", 1000)
+        await browser.click(".addtocart-button", 1);
+        await browser.waitForNavigation(navigationTimeout);
+        await browser.assert.text(".PricediscountedPriceWithoutTax", /\d{2},\d{2}\s\€/) // e.g. 27,99 €
+        console.log("Price Per Item - OK")
+        await browser.assert.text(".PricesalesPrice", /\d{3},\d{2}\s\€/) // e.g 237,99 €
+        console.log("Total Price - OK")
+        await browser.click(".vm2-remove_from_cart")
+        await browser.waitForNavigation(navigationTimeout);
+        await browser.assert.text(".PricebillTotal", "0,00 €")
+        console.log("Empty Basket - OK")
+        await browser.close();
+    }catch(err){}
+    await browser.close()
+}
+
+main().then(() => {
+    console.log("Finished");
+}).catch((err) => {
+    console.log(err);
+})
--- index.md
+++ index.md
@@ -1,3 +1,7 @@
+---
+title: Wendigo And Other Monsters - angrykoala
+---
+
 class: center, middle
 
 # Wendigo
@@ -13,38 +17,72 @@ _@angrykoala_
 
 * **GitHub:** https://github.com/angrykoala
 
-Software developer, koala fan and developer of [Wendigo](https://github.com/angrykoala/wendigo)
+Software developer, koala fan and OS contributor.
 
 ---
 
 ## What is Browser Automation?
 
 ```js
-
 browser.open("www.google.com");
+
 browser.click(".button");
+
 browser.type("Hello there");
 ```
+
+---
+## Why
+
+* **Web Scraping**
+
+* **Services Automation**
+
+* **Testing**
 
 ---
 
 ## Challenges
 
-* Performance.
-* Unreliability (race conditions, bugs, complexity, ...).
-* APIs out of our control.
+* **Performance.**
+
+* **Unreliability (race conditions, bugs, complexity, ...).**
+
+* **APIs out of our control.**
 
 ---
 
 ## How To
 
-* **Create a Headless Browser**
+1. **Create a Headless Browser.**
 
-* **Use Webdriver** (https://www.w3.org/TR/webdriver1)
+2. **Use Webdriver.** (https://www.w3.org/TR/webdriver1)
 
-* **Use Browser-specific tools**
+3. **Use Browser-specific tools.**
 
+---
 
-## Workshop
+## Monstrous Tools
+
+* **Selenium** - https://www.seleniumhq.org
+
+* **Puppeteer** - https://github.com/GoogleChrome/puppeteer
+
+* **Cypress.io** - https://www.cypress.io
+
+--
+count: false
+
+* **Wendigo** - https://github.com/angrykoala/wendigo  
+
+<img src="./static/wendigo.png" height="150">
+
+---
+class: center, middle
 
 ## Examples
+
+---
+class: center, middle
+
+## Workshop
